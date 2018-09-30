2e92e730951524acc02c0e5ba8c7f57cb31ed295
ZharkeyMVP <sirapob.meech@gmail.com>
2018-09-30 23:31:02 +0700

    5555

---
+++ src/components/gunHeader.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+class GunHeader extends Component {
+    render() {
+        return (
+            <header className="background">
+                <h1 className="hero-logo">
+                    M A N A G E M A T E
+        </h1>
+                <div className="mini-background">
+
+                </div>
+            </header>
+
+
+        );
+    }
+}
+
+export default GunHeader;
