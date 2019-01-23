507dde3c385059887033c4712c0a1652b2737797
tanggkwaaa <tk_tang-kwa@hotmail.com>
2019-01-23 17:23:28 +0700

    lll

---
+++ src/components/MyCalendar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../App.css';
+import { Router, Route, IndexRoute, browserHistory, Link } from 'react-router';
+class MyCalendar extends Component {
+    render() {
+        return (
+            <div>
+                <div className="tangkwaTitle"><h4><b>MY CALENDAR</b></h4></div>
+                <Link to='MyCalendarCreateEvent' ><button type="submit" value="event" className="createEvent">CREATE EVENT</button></Link>
+            </div>
+        );
+    }
+}
+
+export default MyCalendar;
