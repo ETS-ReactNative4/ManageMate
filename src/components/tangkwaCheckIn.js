dd0deaf77167ede3f506164a047b19571d8fa321
SirapobGun <sirapobmech@gmail.com>
2019-01-29 18:22:05 +0700

    git commit -m "ewewew"

--- src/components/tangkwaCheckIn.js
+++ src/components/tangkwaCheckIn.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import { Router, Route, IndexRoute, browserHistory, Link } from 'react-router';
 import check from '../Image/check.png'
+import axios from 'axios'; 
 
 const FormHeader = props => {
     return (
@@ -17,14 +18,73 @@ const FormHeader = props => {
         </React.Fragment>
     )
 }
+
+
+
 class TangkwaCheckIn extends Component {
+    constructor() {
+        super()
+    
+        this.state = {
+          latitude: 0,
+          longitude: 0,
+          showLocated: false,
+          address :''
+        }
+    
+        
+      }
+      
+      componentDidMount() {
+     this.setaddress()
+      }
+
+    setaddress = () => {
+        if (navigator.geolocation) { //check if geolocation is available
+            navigator.geolocation.getCurrentPosition(function(position){
+              console.log(position.coords.latitude);
+            //   axios.get( "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude+ "," + position.coords.longitude +"&key=AIzaSyAO5c7iTq4pJLrL8AFRu8z6dIKUu5J05ko", function(data) {
+            //     console.log(data.results[0].formatted_address);
+             
+            //   })
+            //   this.setState ({
+            //     latitude : position.coords.latitude,
+            //     longitude : position.coords.longitude,
+                
+                
+            // })
+             
+            });   
+            
+        }
+    }
+    
+    
+       
+
+            
+      
+      handleSetTrue = () => {
+this.setState({showLocated:true})
+console.log("onclick",this.state.showLocated)
+
+    }
 
     render() {
+        console.log("this is state",this.state)
         return (
             <div className="App">
                 <FormHeader />
-                <div className="tangkwaTitle"><h4>CHECK IN : <img src={check} width="50" height="50" className="checkpng"/></h4></div>
+                <div className="tangkwaTitle"><h4>CHECK IN : <img src={check} width="50" height="50" className="checkpng"  onClick={this.handleSetTrue}/></h4></div>
+                {this.state.showLocated && <div>
+                    <p>Latitude is {this.state.latitude}</p>
+                    <p>Longitude is {this.state.longitude}</p>
+                    <p>address is {this.state.address}</p>
+                    {console.log("la",this.state.latitude)}
+
+                </div>}
                 <div>
+
                     <button type="submit" value="Submit" className="Submit">Submit</button>
                     <button type="submit" value="Cancel" className="Cancel">Cancel</button>
                 </div>
@@ -32,4 +92,5 @@ class TangkwaCheckIn extends Component {
         );
     }
 }
+
 export default TangkwaCheckIn;
