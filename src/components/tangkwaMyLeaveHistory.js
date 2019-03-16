972c15c3b7082e64c6c911483be49cca8c435b9a
tanggkwaaa <tk_tang-kwa@hotmail.com>
2019-03-16 18:11:49 +0700

    llllll

--- src/components/tangkwaMyLeaveHistory.js
+++ src/components/tangkwaMyLeaveHistory.js
@@ -11,8 +11,14 @@ class TangkwaMyLeaveHistory extends Component {
         super(props);
         this.state = {
             people: [],
-            staffid : '1'
+            staffid: '1',
+            Save: "All"
         }
+    }
+    handleSetValue = (event) => {
+        console.log(this.state.Save)
+        this.setState({ Save: event.target.value })
+
     }
     componentDidMount() {
         axios.get(`https://managemate.azurewebsites.net/api/Leave/GetLeaveInfoByStaffID?staffId=${this.state.staffid}`)
@@ -26,6 +32,16 @@ class TangkwaMyLeaveHistory extends Component {
         return (
             <div className="App">
                 <div className="tangkwaTitle"><h4><b>MY LEAVE </b></h4></div>
+                <div className="selecttype">
+             
+                    <select onChange={this.handleSetValue} >
+                        <option value="All">Show All</option>
+                        <option value="Approved">APPROVED</option>
+                        <option value="Pending">PENDING</option>
+                        <option value="Rejected">REJECTED</option>
+
+                    </select>
+                </div>
                 <div className="row flex-container tangkwaSetTable">
                     <div className="tkflex-1"><p>STATUS</p></div>
                     <div className="tkflex-1"><p>LEAVE ID</p></div>
@@ -33,14 +49,20 @@ class TangkwaMyLeaveHistory extends Component {
                     <div className="tkflex-2"><p>LEAVING DATE</p></div>
                     <div className="tkflex-1"><p>MANAGE BY</p></div>
                 </div>
-                {this.state.people.map(people => (
-                    <div className="row flex-container tangkwaSetData">
-                        <div className="tkflex-1"><div className={`${people.leaveStatus == 'Approved' ? 'tangkwaSetApprove' : people.leaveStatus == 'Pending' ? 'tangkwaSetPending' : 'tangkwaSetReject'}`}><p><b>{people.leaveStatus}</b></p></div></div>
-                        <Link to={`/LeaveDetail/${people.leaveID}`} className="tkflex-1"><div><p>LEV{_.padStart(people.leaveID, 5, '0')}</p></div></Link>
-                        <div className="tkflex-2"><p>{people.firstnameEN} {people.lastnameEN}</p></div>
-                        <div className="tkflex-2"><p>{moment(people.leaveStartDateTime).format('DD-MM-YYYY')}</p></div>
-                        <div className="tkflex-1"><p>{people.approvedBy}</p></div>
-                    </div>))}
+                {
+                    this.state.people.filter((people) => {
+                        if (this.state.Save === 'All') {
+                            return true
+                        }
+                        return people.leaveStatus === this.state.Save
+                    }).map((people) =>
+                        <div className="row flex-container tangkwaSetData">
+                            <div className="tkflex-1"><div className={`${people.leaveStatus == 'Approved' ? 'tangkwaSetApprove' : people.leaveStatus == 'Pending' ? 'tangkwaSetPending' : 'tangkwaSetReject'}`}><p><b>{people.leaveStatus}</b></p></div></div>
+                            <Link to={`/LeaveDetail/${people.leaveID}`} className="tkflex-1"><div><p>LEV{_.padStart(people.leaveID, 5, '0')}</p></div></Link>
+                            <div className="tkflex-2"><p>{people.firstnameEN} {people.lastnameEN}</p></div>
+                            <div className="tkflex-2"><p>{moment(people.leaveStartDateTime).format('DD-MM-YYYY')}</p></div>
+                            <div className="tkflex-1"><p>{people.approvedBy}</p></div>
+                        </div>)}
             </div>
         );
     }
